0d87f57096992c81817c6dbbe20b51df73c89c12
Nikolay Kirillov <kirillovnik2001@gmail.com>
2022-04-27 15:00:43 +0300

    dashboard_upd

--- tech_school_backend/dashboard_app/views.py
+++ tech_school_backend/dashboard_app/views.py
@@ -27,10 +27,121 @@ df_membership = pd.DataFrame(membership)
 df_incp = pd.DataFrame(incp)
 df_calplan = pd.DataFrame(calplan)
 
-df = pd.merge(df_grades[['grade_type', 'grade', 'attendance', 'student_id']], df_membership[['group_id', 'student_id']], left_on='student_id', right_on='student_id')
+df = pd.merge(df_grades[['grade_type', 'grade', 'attendance', 'student_id']], df_membership[['group_id', 'student_id']],
+              left_on='student_id', right_on='student_id')
 df = pd.merge(df, df_groups[['id', 'name', 'program_id']], left_on='group_id', right_on='id')
-df_ = pd.merge(df_incp[['calendarplan_id', 'program_id']], df_calplan[['id', 'year']], left_on='calendarplan_id', right_on='id')
+df_ = pd.merge(df_incp[['calendarplan_id', 'program_id']], df_calplan[['id', 'year']], left_on='calendarplan_id',
+               right_on='id')
 df = pd.merge(df, df_, left_on='program_id', right_on='program_id')
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
+
+fig_grades = px.bar(df[df["grade_type"] == 'g'], x='grade', y='student_id', title='Оценки')
+pie_attendance = px.pie(df[df["grade_type"] == 'a'], names='attendance', title="Процент посещений")
+# attendance = len(df[df["grade_type"] == 'a']["attendance"]) * pd.to_numeric(df[df["grade_type"] == 'a']["attendance"]).sum() / 100
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = DjangoDash('dashboard', external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(
+    children=[
+        html.Div(children='year', style={'fontSize': "24px"}, className='menu-title'),
+        dcc.Dropdown(
+            id='year-filter',
+            options=[
+                {'label': year, 'value': year}
+                for year in df.year.unique()
+            ],
+            className='dropdown'
+        ),
+        html.Div(children='group', style={'fontSize': "24px"}, className='menu-title'),
+        dcc.Dropdown(
+            id='group-filter',
+            className='dropdown'
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id='bar_1',
+                        figure=fig_grades
+                    ), style={'width': '50%', 'display': 'inline-block'}
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id='pie_attendance',
+                        figure=pie_attendance
+                    ), style={'width': '50%', 'display': 'inline-block'}
+                ),
+                html.Div(
+                    children=generate_table(df)
+                )
+            ]
+        )
+    ]
+)
+
+
+@app.callback(
+    [Output("group-filter", "options")],
+    [Input("year-filter", "value")]
+)
+def update_options(value):
+    options = [
+                  {'label': group_name, 'value': group}
+                  for group in df[df['year'] == value].group_id.unique()
+                  for group_name in df[df['group_id'] == group].name.unique()
+              ],
+    return options
+
+
+@app.callback(
+    Output("bar_1", "figure"),
+    [Input("group-filter", "value")],
+)
+def update_charts(group):
+    filtered_data = df[df["group_id"] == group]
+    filtered_data = filtered_data[filtered_data["grade_type"] == 'g']
+
+    bar = px.bar(
+        filtered_data,
+        x='grade',
+        y='student_id',
+        title='Оценки',
+    )
+    return bar
+
+@app.callback(
+    Output("pie_attendance", "figure"),
+    [Input("group-filter", "value")],
+)
+def update_charts(group):
+    filtered_data = df[df["group_id"] == group]
+    filtered_data = filtered_data[filtered_data["grade_type"] == 'a']
+
+    bar = px.pie(
+        filtered_data,
+        names='attendance',
+        title="Процент посещений",
+    )
+    return bar
+def update_value(group):
+    filtered_data = df[df["group_id"] == group]
+    group_attendance = len(filtered_data[filtered_data["grade_type"] == 'a']["attendance"]) * pd.to_numeric(filtered_data[filtered_data["grade_type"] == 'a']["attendance"]).sum() / 100
+
+    return group_attendance
 
 
 class DashboardView(TemplateView):
@@ -40,150 +151,3 @@ class DashboardView(TemplateView):
     """
 
     template_name = "dashboard.html"
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context.update(site.each_context(self.request))
-    #
-    #     context["headings1"] = [  # таблица средних баллов
-    #         'студент',
-    #         'предмет',
-    #         'средний балл',
-    #     ]
-    #
-    #     context["headings2"] = [  # таблица посещаемости
-    #         'студент',
-    #         'предмет',
-    #         'present',
-    #         'absent',
-    #         '% present',
-    #     ]
-    #
-    #     context["headings3"] = [  # таблица totals
-    #         'студент',
-    #         'часов посетил',
-    #         'всего часов',
-    #         '% часов',
-    #         'средние оценки',
-    #         'среднее по всем оценкам'
-    #     ]
-    #
-    #     date = self.request.GET.get("date")
-    #     group = self.request.GET.get("group")
-    #
-    #     groups = Group.objects.all()  # все группы
-    #     context["groups"] = groups
-    #
-    #     queryset = Grade.objects.all()  # queryset: все оценки
-
-    # queryset = Grade.objects.all().values()
-    # groups = Group.objects.all().values()
-    # membership = Membership.objects.all().values()
-    #
-    # df_grades = pd.DataFrame(queryset)
-    # df_groups = pd.DataFrame(groups)
-    # df_membership = pd.DataFrame(membership)
-    #
-    # df = pd.merge(df_grades, df_membership, left_on='student_id', right_on='student_id')
-    # df = pd.merge(df, df_groups, left_on='group_id', right_on='id')
-
-    def generate_table(dataframe, max_rows=10):
-        return html.Table(
-            # Header
-            [html.Tr([html.Th(col) for col in dataframe.columns])] +
-
-            # Body
-            [html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))]
-        )
-
-    # df = pd.read_csv(r"C:\Users\kiril\Desktop\pBNN\Data\result_table.csv", delimiter=";").copy()
-
-    # df_people = df[['name', 'group']].groupby('group').count().rename(columns={"name": "people"}).reset_index()
-    #
-    # fig = px.pie(df_people, values='people', names='group', title='Количество студентов в группах')
-
-    # df_question1 = df[['name', 'stud_1']].groupby('stud_1').count().rename(columns={"name": "people"}).reset_index()
-    #
-    # fig_1 = px.bar(df_question1, x='stud_1', y='people', title='Первый вопрос')
-    #
-    # df_question2 = df[['name', 'stud_2']].groupby('stud_2').count().rename(columns={"name": "people"}).reset_index()
-    #
-    # fig_2 = px.bar(df_question2, x='stud_2', y='people', title='Второй вопрос')
-
-    fig_grades = px.bar(df[df["grade_type"] == 'g'], x='grade', y='student_id', title='Оценки')
-
-    # table = go.Figure(data=[go.Table(header=dict(values=list(df.columns),
-    #                                              fill_color='paleturquoise',
-    #                                              align='left'),
-    #                                  cells=dict(values=[df.Rank, df.State, df.Postal, df.Population],
-    #                                             fill_color='lavender',
-    #                                             align='left'))])
-
-    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-    app = DjangoDash('dashboard', external_stylesheets=external_stylesheets)
-
-    app.layout = html.Div(
-        children=[
-            html.Div(children='group', style={'fontSize': "24px"}, className='menu-title'),
-            dcc.Dropdown(
-                id='group-filter',
-                options=[
-                    {'label': group, 'value': group}
-                    for group in df.name.unique()
-                ],
-                # multi=True,
-                value='M-227',
-                className='dropdown'
-            ),
-
-            html.Div(
-                children=[
-                    html.Div(
-                        children=dcc.Graph(
-                            id='bar_1',
-                            figure=fig_grades
-                        ), style={'width': '50%', 'display': 'inline-block'}
-                    ),
-                    html.Div(
-                        children=generate_table(df)
-                    )
-                ]
-            )
-        ]
-    )
-
-    @app.callback(
-        Output("bar_1", "figure"),
-        [Input("group-filter", "value")],
-    )
-    def update_charts(group):
-        filtered_data = df[df["name"] == group]
-
-        bar = px.bar(
-            filtered_data,
-            x='grade',
-            y='student_id',
-            title='Оценки',
-        )
-        return bar
-    #
-    # @app.callback(
-    #     Output("bar_2", "figure"),
-    #     [Input("group-filter", "value")],
-    # )
-    # def update_charts(group):
-    #     df = pd.read_csv(r"C:\Users\kiril\Desktop\pBNN\Data\result_table.csv", delimiter=";").copy()
-    #     filtered_data = df[df["group"] == group]
-    #     filtered_data = filtered_data[['name', 'stud_2']].groupby('stud_2').count().rename(
-    #         columns={"name": "people"}).reset_index()
-    #
-    #     bar = px.bar(
-    #         filtered_data,
-    #         x='stud_2',
-    #         y='people',
-    #         title='Второй вопрос',
-    #     )
-    #     return bar
